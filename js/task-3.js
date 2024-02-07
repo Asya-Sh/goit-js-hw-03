2b4e6ae6309736c6d93583d29dd892272233f9b1
Asya-Sh <151557286+Asya-Sh@users.noreply.github.com>
2024-02-07 17:20:52 +0200

    Initial commit

---
+++ js/task-3.js
@@ -0,0 +1,16 @@
+function checkForSpam(message) {
+  const normalizeMessage = message.toLowerCase();
+  
+  return (
+    normalizeMessage.includes('spam') ||
+    normalizeMessage.includes('sale')
+  );
+}
+
+console.log(checkForSpam('Latest technology news')); // false
+console.log(checkForSpam('JavaScript weekly newsletter')); // false
+console.log(checkForSpam('Get best sale offers now!')); // true
+console.log(checkForSpam('Amazing SalE, only tonight!')); // true
+console.log(checkForSpam('Trust me, this is not a spam message')); // true
+console.log(checkForSpam('Get rid of sPaM emails. Our book in on sale!')); // true
+console.log(checkForSpam('[SPAM] How to earn fast money?')); // true
